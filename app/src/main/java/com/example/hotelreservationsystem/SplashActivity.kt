7ae815a8425e6543fd5ae023f619f1c794af0105
Bikash <hujdarbikash000@gmail.com>
2023-04-30 23:36:55 +0545

    Registration complete

--- app/src/main/java/com/example/hotelreservationsystem/SplashActivity.kt
+++ app/src/main/java/com/example/hotelreservationsystem/SplashActivity.kt
@@ -1,15 +1,40 @@
 package com.example.hotelreservationsystem
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.ProgressBar
+import com.github.ybq.android.spinkit.style.ThreeBounce
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // managing the spinkit library
+        supportActionBar?.hide()
+
+        // hidding atatus bar
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val progressBar = findViewById<View>(R.id.spin) as ProgressBar
+        val threeBounce = ThreeBounce()
+        progressBar.indeterminateDrawable = threeBounce
+
 
         // setting up post delayed in lap
         Handler(Looper.getMainLooper()).postDelayed(
