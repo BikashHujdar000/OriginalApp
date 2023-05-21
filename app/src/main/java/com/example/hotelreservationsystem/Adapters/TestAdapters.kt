f62696aa36b849666c95727b83e79f135677f66e
Bikash <hujdarbikash000@gmail.com>
2023-05-21 08:54:31 +0545

    This is being Commited

--- app/src/main/java/com/example/hotelreservationsystem/Adapters/TestAdapters.kt
+++ app/src/main/java/com/example/hotelreservationsystem/Adapters/TestAdapters.kt
@@ -12,15 +12,31 @@ import com.example.hotelreservationsystem.TestModels.DataModel
 
 class TestAdapters (val context :Context,val data: List<DataModel>):RecyclerView.Adapter<TestAdapters.MyViewHolder>() {
 
-    class MyViewHolder (itemview:View) : RecyclerView.ViewHolder(itemview)
+lateinit var  mlistner :onItemClickListner
+       interface onItemClickListner{
+           fun onItemClick(position: Int)
+       }
+
+    fun setOnItemClickListner(listner: onItemClickListner)
     {
+        mlistner = listner
+    }
+    class MyViewHolder(itemview: View,listner: onItemClickListner) : RecyclerView.ViewHolder(itemview)
+    {
+
         val name = itemview.findViewById<TextView>(R.id.hotel_sample_name)
         val location = itemview.findViewById<TextView>(R.id.hotel_sample_country_name)
+        init {
+            itemview.setOnClickListener {
+                listner.onItemClick(adapterPosition)
+            }
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.test_layout_for_model_view,parent,false)
-        return  MyViewHolder(view)
+        return  MyViewHolder(view,mlistner)
     }
 
     override fun getItemCount(): Int {
@@ -30,36 +46,10 @@ class TestAdapters (val context :Context,val data: List<DataModel>):RecyclerView
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
       holder.name.text = data.get(position).name
         holder.location.text = data.get(position).location
+
+
     }
 
 }
 
 
-
-
-//lass NewsAdapter(val context: Context,val article: List<Article>) : RecyclerView.Adapter<NewsAdapter.MyViewHolder>(){
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-//        val view = LayoutInflater.from(context).inflate(R.layout.smaple,parent,false)
-//        return  MyViewHolder(view)
-//    }
-//
-//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-//        holder.title.text=article[position].title
-//        holder.description.text=article[position].description
-//        Glide.with(context).load(article[position].urlToImage).into(holder.image)
-//
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return  article.size
-//    }
-//
-//    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-//        val title = itemView.findViewById<TextView>(R.id.txt_title)
-//        val description = itemView.findViewById<TextView>(R.id.txt_description)
-//        val image= itemView.findViewById<ImageView>(R.id.imageView2)
-//
-//    }
-//
-//
--- app/src/main/java/com/example/hotelreservationsystem/UserActivity.kt
+++ app/src/main/java/com/example/hotelreservationsystem/UserActivity.kt
@@ -4,8 +4,11 @@ import ScrollHandler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import com.example.hotelreservationsystem.Fragments.UserBookingFragment
 import com.example.hotelreservationsystem.Fragments.UserHomeFragment
@@ -20,6 +23,16 @@ class UserActivity : AppCompatActivity() {
 
        binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        // setiing the status baar and icon
+
+        val window =this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.white)
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+        }
         val bottomBar = binding.bottomNavigationBar
         val layoutParams = bottomBar .layoutParams as CoordinatorLayout.LayoutParams
         layoutParams.behavior = ScrollHandler()
@@ -29,13 +42,16 @@ class UserActivity : AppCompatActivity() {
                 0->{
 
                     val  fragment= UserBookingFragment()
+                  bottomBar.visibility=View.GONE
                     switchFragment(fragment)
+
 
                 }
                 1->{
 
                     val fragment = UserHomeFragment()
                     switchFragment(fragment)
+
                 }
                 2->{
 
