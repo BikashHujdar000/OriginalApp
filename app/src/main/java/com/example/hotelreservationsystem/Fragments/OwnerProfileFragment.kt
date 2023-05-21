2de2e0654ba86607779b338c250b5b12e3283fbb
Bikash <hujdarbikash000@gmail.com>
2023-05-21 13:34:33 +0545

    This is Sucess done

--- app/src/main/java/com/example/hotelreservationsystem/Fragments/OwnerProfileFragment.kt
+++ app/src/main/java/com/example/hotelreservationsystem/Fragments/OwnerProfileFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.hotelreservationsystem.R
+import com.example.hotelreservationsystem.databinding.FragmentBookNowBinding
+import com.example.hotelreservationsystem.databinding.FragmentOwnerProfileBinding
 
 
 class OwnerProfileFragment : Fragment() {
+    lateinit var  binding: FragmentOwnerProfileBinding
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentOwnerProfileBinding.inflate(layoutInflater,container,false
+        )
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_owner_profile, container, false)
+
+
+
+        binding
+            .updateHotel.setOnClickListener{
+                Navigation.findNavController(it).navigate(R.id.action_ownerProfileFragment_to_ownerHomeFragment)
+            }
+        return binding.root
     }
 
 
